bd110bfe2e2f7ec54bbfee83a22d7d06a97b9403
吳容陞 <s1121126@o365st.pu.edu.tw>
2024-12-09 15:35:59 +0800

    v2_吳容陞

--- app/src/main/java/com/example/s1121126/MainActivity.kt
+++ app/src/main/java/com/example/s1121126/MainActivity.kt
@@ -3,45 +3,47 @@ package com.example.s1121126
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.s1121126.ui.theme.S1121126Theme
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
-            S1121126Theme {
-                Scaffold( modifier = Modifier.fillMaxSize() ) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+            MyApp()
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    S1121126Theme {
-        Greeting("Android")
+fun MyApp() {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color(0xff95fe95)),
+        contentAlignment = Alignment.Center
+    ) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(text = "2024期末上機考(資管二B吳容陞)", fontSize = 24.sp, color = Color.Black)
+            Spacer(modifier = Modifier.height(16.dp))
+            Text(text = "圖片：A班同學請放class_a， B班同學請放class_b", fontSize = 18.sp, color = Color.Black)
+            Spacer(modifier = Modifier.height(16.dp))
+            Text(text = "遊戲持續時間 0 秒", fontSize = 18.sp, color = Color.Black)
+            Spacer(modifier = Modifier.height(8.dp))
+            Text(text = "您的成績 0 分", fontSize = 18.sp, color = Color.Black)
+            Spacer(modifier = Modifier.height(32.dp))
+            Button(onClick = { (this@MainActivity as? ComponentActivity)?.finish() }) {
+                Text(text = "結束App", fontSize = 16.sp)
+            }
+        }
     }
 }
